290e99dc8b3c28808cd62ba794418d623435699c
Emilio Jorge <emilio@jorge.se>
2021-03-24 17:07:48 +0100

    Close to draft

--- run_experiment.py
+++ run_experiment.py
@@ -4,6 +4,7 @@ import importlib.util
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--agentfile", type=str, help="file with Agent object", default="agent.py")
+parser.add_argument("--env", type=str, help="Environment", default="FrozenLake-v0")
 args = parser.parse_args()
 
 spec = importlib.util.spec_from_file_location('Agent', args.agentfile)
@@ -11,9 +12,18 @@ agentfile = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(agentfile)
 reward = []
 
+try:
+    env = gym.make(args.env)
+    print("Loaded ", args.env)
+except:
+    print(args.env +':Env')
+    gym.envs.register(
+        id=args.env + "-v0",
+        entry_point=args.env +':Env',
+    )
+    env = gym.make(args.env + "-v0")
+    print("Loaded", args.env)
 
-
-env = gym.make("FrozenLake-v0")
 action_dim = env.action_space.n
 state_dim = env.observation_space.n
 
@@ -26,8 +36,6 @@ for _ in range(10000):
     observation, reward, done, info = env.step(action)
     agent.observe(observation, reward, done)
 
-
-    print(reward)
-    if done: 
+    if done:
         observation = env.reset() 
 env.close()
