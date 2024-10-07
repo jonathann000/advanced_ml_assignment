84d0a5e7aceb93ba972eb07313fe3ebbd4d3bce9
jonathann000 <97891351+jonathann000@users.noreply.github.com>
2024-10-07 16:02:36 +0200

    update agent

--- agent.py
+++ agent.py
@@ -2,14 +2,42 @@ import numpy as np
 
 class Agent(object):
     """The world's simplest agent!"""
-    def __init__(self, state_space, action_space):
+    def __init__(self, state_space, action_space, alpha=0.1, epsilon=0.05, gamma=0.95):
         self.action_space = action_space
         self.state_space = state_space
+        self.alpha = alpha
+        self.epsilon = epsilon
+        self.gamma = gamma
+        self.q_values = np.ones((state_space, action_space))
+        self.prev_observation = None
+        self.prev_action = None
+
+        self.stupid_flag = False
+
+
 
     def observe(self, observation, reward, done):
         #Add your code here
-        pass
-    def act(self, observation):
-        #Add your code here
+        if not done:
+            self.q_values[self.prev_observation, self.prev_action] += self.alpha * (reward + self.gamma * np.max(self.q_values[observation]) - self.q_values[self.prev_observation, self.prev_action])
+        else:
+            print("tjenare")
+            self.q_values[self.prev_observation, self.prev_action] += self.alpha * (reward - self.q_values[self.prev_observation, self.prev_action])
+        
+        print(self.q_values)
 
-        return np.random.randint(self.action_space)
+    
+        
+    def act(self, observation):
+        if not self.stupid_flag:
+            observation = observation[0]
+            self.stupid_flag = True
+        if np.random.rand() < self.epsilon:
+            self.prev_action = np.random.choice(range(self.action_space))
+        
+        else:
+            self.prev_action = np.argmax(self.q_values[observation])
+
+        self.prev_observation = observation
+
+        return self.prev_action
